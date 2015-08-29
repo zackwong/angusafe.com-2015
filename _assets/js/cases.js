ba42693e60ffee20655278d7dcb9ddc4d9dd5015
zackwong <hzzzoo@126.com>
2015-08-29 14:53:37 +0800

    up

--- _assets/js/cases.js
+++ _assets/js/cases.js
@@ -17,6 +17,24 @@ jQuery(function($) {
       });
     }
   });
+  $("#casesz .case_item").each(function(a, b) {
+    return $(b).magnificPopup({
+      items: {
+        src: '<div style="text-align: center;"><iframe class="vid" height=400 width=720 src="' + $(b).find('a').data('iframe') + '" frameborder=0 allowfullscreen></iframe></div>'
+      },
+      type: "inline"
+    });
+  });
+  $('.social-media, .clicktozoom').magnificPopup({
+    type: "image",
+    zoom: {
+      enabled: true,
+      duration: 300,
+      opener: function(element) {
+        return element;
+      }
+    }
+  });
   return $('.openinline').magnificPopup({
     type: 'inline'
   });
